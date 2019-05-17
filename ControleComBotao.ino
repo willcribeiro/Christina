8e5edbeab9d2c013f4ec9dcc1bef5f0299a5179c
willcribeiro <william-cribeiro@bct.ect.ufrn.br>
2019-05-17 11:28:57 -0300

    add controler functions

---
+++ ControleComBotao.ino
@@ -0,0 +1,20 @@
+int botao = 2;
+int led = 8;
+int estado = 0;
+void setup() {
+ Serial.begin(19200);
+ pinMode(botao,INPUT);
+ pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  estado = digitalRead(botao);
+  if(estado == HIGH){
+    digitalWrite(led, HIGH);
+    Serial.write(0b00100000);
+    Serial.write(0b11111111);
+    Serial.write(0b11111111);
+    }
+  digitalWrite(led, LOW);
+}
