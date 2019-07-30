1889c88c409dc692d771be9c9e519115bdf158e7
willcribeiro <william-cribeiro@bct.ect.ufrn.br>
2019-07-30 12:20:08 -0300

    Implementado controle de velocidade

--- controle/controle.ino
+++ controle/controle.ino
@@ -27,7 +27,9 @@ void funcoes(char nome[8]) {
   else if (nome[0] == '5') {
     ClearPosition(nome);    //Limpar encoder
   }
-
+  else if (nome[0] == '8') {
+    SpeedMax(nome);    //Define speed maxima
+  }
 
 }
 void ClearPosition(char comando[8]) {
@@ -66,7 +68,7 @@ void travel(char comando[8]) {
   }
 
   int k1 = rotacao & 0x00FF;
-  int k2 = rotacao>>8;
+  int k2 = rotacao >> 8;
 
   byte x = k2; //max 254
   byte y = k1; //max 255
@@ -83,7 +85,7 @@ void travel(char comando[8]) {
     Serial.write(x);
     Serial.write(y);
 
-    if (comando[2] == '1') {   //Quantidade de giros 
+    if (comando[2] == '1') {   //Quantidade de giros
       Rot1 = Rot1 - numero;
       Rot2 = Rot2 - numero;
     }
@@ -94,8 +96,8 @@ void travel(char comando[8]) {
   }
   else if (comando[1] == '1') { //Mover a roda 1
     Serial.write(0b00100100);
-   Serial.write(0b00000000); //Teste de movimentacao
-    Serial.write(0b00100101);
+    Serial.write(~x); //Teste de movimentacao
+    Serial.write(~y);
 
     if (comando[2] == '1') {
       Rot1 = Rot1 - numero;
@@ -107,8 +109,8 @@ void travel(char comando[8]) {
   }
   else if (comando[1] == '2') { //Mover a roda 2
     Serial.write(0b00100010);
-    Serial.write(0b00000000); //Teste de movimentacao
-    Serial.write(0b01001010);
+    Serial.write(x); //Teste de movimentacao
+    Serial.write(y);
 
 
     if (comando[2] == '1') {
@@ -121,6 +123,34 @@ void travel(char comando[8]) {
   }
 
 }
+void SpeedMax(char comando[8]) {
+
+  int numero, rotacao;
+  numero = (comando[3] - '0') * (pow(10, 4)) + (comando[4] - '0') * (pow(10, 3)) + (comando[5] - '0') * (pow(10, 2)) + (comando[6] - '0') * (10) + (comando[7] - '0'); //transformando o numero em um só
+  int k1 = numero & 0x00FF;
+  int k2 = numero >> 8;
+
+  byte x = k2; //max 254
+  byte y = k1; //max 255
+
+  if (comando[1] == '0') {
+    Serial.write(0b01000001);
+    Serial.write(x);
+    Serial.write(y);
+  }
+  else if (comando[1] == '1') {
+    Serial.write(0b01000100);
+    Serial.write(x);
+    Serial.write(y);
+  }
+
+  else if (comando[1] == '2') {
+    Serial.write(0b01000010);
+    Serial.write(x);
+    Serial.write(y);
+  }
+
+}
 void setup() {
   Serial.begin(19200);
 
@@ -129,25 +159,25 @@ void setup() {
 void loop() {
   char x;
   //-----------------TESTE DE MOVIMENTACAO PRE DETERMINADA------------------------
-  /*char movimento1[8], movimento2[8];   
-  movimento1[0] = '4';
-  movimento1[1] = '0';
-  movimento1[2] = '0';
-  movimento1[3] = '0';
-  movimento1[4] = '0';
-  movimento1[5] = '0';
-  movimento1[6] = '0';
-  movimento1[7] = '3';
+  /*char movimento1[8], movimento2[8];
+    movimento1[0] = '4';
+    movimento1[1] = '0';
+    movimento1[2] = '0';
+    movimento1[3] = '0';
+    movimento1[4] = '0';
+    movimento1[5] = '0';
+    movimento1[6] = '0';
+    movimento1[7] = '3';
 
-  movimento2[0] = '1';
-  movimento2[1] = '0';
-  movimento2[2] = '0';
-  movimento2[3] = '0';
-  movimento2[4] = '0';
-  movimento2[5] = '0';
-  movimento2[6] = '0';
-  movimento2[7] = '0';
-  while (Serial.available() > 0) {
+    movimento2[0] = '1';
+    movimento2[1] = '0';
+    movimento2[2] = '0';
+    movimento2[3] = '0';
+    movimento2[4] = '0';
+    movimento2[5] = '0';
+    movimento2[6] = '0';
+    movimento2[7] = '0';
+    while (Serial.available() > 0) {
     x = Serial.read();
 
     if (x == '8') {   //Wait for all bits(8 bits)
@@ -167,7 +197,7 @@ void loop() {
 
 
     }
-  }*/
+    }*/
 
   while (Serial.available() > 0) {
     x = Serial.read();
@@ -177,5 +207,5 @@ void loop() {
       funcoes(Protocolo);
       i = 0;
     }
-    }
+  }
 }
