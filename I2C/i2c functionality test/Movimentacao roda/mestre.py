19ef70e3116089c7c40aec28151fc276019482d3
willcribeiro <william-cribeiro@bct.ect.ufrn.br>
2020-01-13 16:35:30 -0300

    Test control Christina

---
+++ I2C/i2c functionality test/Movimentacao roda/mestre.py
@@ -0,0 +1,18 @@
+from smbus import SMBus
+
+addr = 0x8
+bus = SMBus(1)
+
+numb = 1
+print("4 para movimentar")
+while numb ==1:
+    ledstate = input(">>>>   ")
+
+    if ledstate ==4:
+        bus.write_byte(addr,0x1)
+    elif ledstate ==0:
+        bus.write_byte(addr,0x0)
+    else:
+        numb = 0
+
+
---
+++ I2C/i2c functionality test/Movimentacao roda/slave/slave.ino
@@ -0,0 +1,90 @@
+#include <Wire.h>
+//Instrucao de Querry
+int QPOS = 0b00001;
+int QSPD = 0b00010;
+int CHFA = 0b00011;
+//Instrucao de acao
+int TRVL = 0b00100;
+int CLRP = 0b00101;
+//Instrucao de conf
+int SREV = 0b00110;
+int STXD = 0b00111;
+int SMAX = 0b01000;
+int SSRR = 0b01001;
+//Motores
+int motor1 = 0b100;
+int motor2 = 0b010;
+int motor0 = 0b000;
+//Variavel Global
+int protocolo;
+
+int CalculoProt(int Nome, int mot ){ //concatenacao de protocolos
+  int x;
+  x = Nome*8 + mot;
+  return x;
+}
+
+void travel(char comando[8]) {
+  int numero, rotacao;
+  numero = (comando[3] - '0') * (pow(10, 4)) + (comando[4] - '0') * (pow(10, 3)) + (comando[5] - '0') * (pow(10, 2)) + (comando[6] - '0') * (10) + (comando[7] - '0'); //transformando o numero em um só
+  rotacao = numero * 36;  //9 para uma rotação
+  //rotacao = numero;
+  if (rotacao > 32767) {
+    rotacao = 32767;
+  }
+  int k1 = rotacao & 0x00FF;
+  int k2 = rotacao >> 8;
+
+  byte x = k2; //max 254
+  byte y = k1; //max 255
+
+  if (comando[2] == '1') {  //Se for negativo, complemento A1
+    x = ~x;
+    y = ~y;
+  }
+  if (comando[1] == '0') {  //Mover  as 2 rodas
+    Serial.write(0b00100100);
+    Serial.write(~x);
+    Serial.write(~y);
+    Serial.write(0b00100010);
+    Serial.write(x);
+    Serial.write(y);
+  }
+  else if (comando[1] == '1') { //Mover a roda 1
+    Serial.write(0b00100100);
+    Serial.write(~x); //Teste de movimentacao
+    Serial.write(~y);
+  }
+  else if (comando[1] == '2') { //Mover a roda 2
+    Serial.write(0b00100010);
+    Serial.write(x); //Teste de movimentacao
+    Serial.write(y);
+  }
+}
+
+
+void setup() {
+  
+  Serial.begin(19200); 
+  Wire.begin(0x8);       
+  Wire.onReceive(receiveEvent);
+  
+}
+
+
+void receiveEvent(int howMany) {
+  while (Wire.available()) { // loop through all but the last
+    int c = Wire.read(); // receive byte as a character
+    if(c==1){
+      Serial.print("ENtrei");
+      char movimento[8] = {'4', '0', '0', '0', '0', '0',  '0', '2'};
+      travel(movimento);
+    }
+  }
+}
+
+void loop() {
+  delay(100);    
+}
+
+
